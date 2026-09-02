e3663c46051cf617511caa471aa16761c8c04096
dlarv <hyrum21344@gmail.com>
2026-09-01 21:17:29 -0400

    StoryManager can now access StoryState during runtime

--- addons/dialogue_nodes/objects/DialogueParser.gd
+++ addons/dialogue_nodes/objects/DialogueParser.gd
@@ -59,10 +59,10 @@ func set_data(new_data: DialogueData) -> void:
 	for var_name in data.variables:
 		variables[var_name] = data.variables[var_name].value
 	#DLARV
-	# for var_name in StoryManager.variables:
-	# 	variables[var_name] = StoryManager.variables[var_name].value
-	#
-	# characters = StoryManager.characters
+	for var_name in StoryManager.variables:
+		variables[var_name] = StoryManager.variables[var_name].value
+	
+	characters = StoryManager.characters
 
 
 ## Starts processing the dialogue data set in [member data], starting with the Start Node with its ID set to [param start_id].
--- addons/dialogue_nodes/objects/StoryManager.gd
+++ addons/dialogue_nodes/objects/StoryManager.gd
@@ -2,55 +2,20 @@
 extends Node
 #
 
-@export var characters: Array[Character]
-@export var variables: Dictionary[String, Dictionary]
-#
-# var _custom_dialog_nodes: Array[String]
-#
-#
-# func add_character(character: Character) -> void:
-# 	characters.append(character)
-# 	character_list_updated.emit()
-#
-#
-# func remove_character(character: Character) -> void:
-# 	var idx: int = StoryManager.characters.find(character)
-# 	StoryManager.characters.remove_at(idx)
-# 	character_list_updated.emit()
-#
-#
-# func new_variable(key: String) -> void:
-# 	if variables.has(key):
-# 		push_error("Could not add var. '%s' already exists" % key)
-# 		return
-# 	variables[key] = {}
-# 	variable_list_updated.emit(get_variable_list())
-#
-#
-# func remove_variable(key: String) -> void:
-# 	if not variables.has(key):
-# 		push_error("Could not remove var. '%s' not found" % key)
-# 		return
-# 	variables.erase(key)
-# 	variable_list_updated.emit(get_variable_list())
-#
-#
-# func rename_variable(old_name: String, new_name: String) -> void:
-# 	if not variables.has(old_name):
-# 		push_error("Could not rename var. '%s' not found" % old_name)
-# 		return
-#
-# 	var data := variables.get(old_name)
-# 	variables.erase(old_name)
-# 	variables[new_name] = data
-#
-# 	variable_list_updated.emit(get_variable_list())
-#
-#
-# func get_variable_list() -> Array[String]:
-# 	return variables.keys()
-#
-#
+@export var characters: Array[Character]:
+	get:
+		return _story_state.characters
+
+@export var variables: Dictionary[String, Dictionary]:
+	get:
+		return _story_state.variables
+
+var _story_state: StoryState
+
+func _enter_tree() -> void:
+	_story_state = ResourceLoader.load("res://story_state.tres")
+
+
 # ## Takes mixed local and global vars and updates global var values
 func update_variables(data: Dictionary) -> void:
 	for key in data:
